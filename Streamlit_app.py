6115d7993ed95fef0fd955d813015e66510bbf1e
RiyaDubal1021 <137475989+RiyaDubal1021@users.noreply.github.com>
2023-06-25 15:41:34 -0700

    Update Streamlit_app.py
    Added more item and symbols

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,8 +4,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry & Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry & Oatmeal')
 
-streamlit.text('Kale, Spinach 3 & Rocket Soomethie')
+streamlit.text('🥗 Kale, Spinach 3 & Rocket Soomethie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑 Avocado Tost')
