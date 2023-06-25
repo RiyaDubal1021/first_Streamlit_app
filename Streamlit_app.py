d2ed98684a2bd1da51b83ba9a38fd8be5fc39fc3
RiyaDubal1021 <137475989+RiyaDubal1021@users.noreply.github.com>
2023-06-25 15:38:31 -0700

    Update Streamlit_app.py
    fixed parenthesis error

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Omega 3 & Blueberry & Oatmeal')
 
 streamlit.text('Kale, Spinach 3 & Rocket Soomethie')
 
-streamlit.text('Hard-Boiled Free-Range Egg'
+streamlit.text('Hard-Boiled Free-Range Egg')
