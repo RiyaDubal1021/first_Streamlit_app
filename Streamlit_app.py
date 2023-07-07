08517544cce805b560b870d8190cc9e0c4af5b9f
RiyaDubal1021 <137475989+RiyaDubal1021@users.noreply.github.com>
2023-07-07 02:10:58 -0700

    Update Streamlit_app.py
    json response

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 streamlit.title('My Parents New Healthy Diner')
 
