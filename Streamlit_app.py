22e0a36a6884cb653f0953cf137126713300e162
RiyaDubal1021 <137475989+RiyaDubal1021@users.noreply.github.com>
2023-07-07 12:23:41 -0700

    Update Streamlit_app.py
    change location of meta data query

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -37,13 +37,16 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
 
+# take the json version of the response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it on the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+#query meta data
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-# take the json version of the response and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
