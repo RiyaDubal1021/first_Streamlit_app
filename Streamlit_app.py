643cfb54c4ef85bd6561e5167257532b77ad1f86
RiyaDubal1021 <137475989+RiyaDubal1021@users.noreply.github.com>
2023-07-07 15:10:41 -0700

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,7 +5,7 @@ import pandas
 import requests
 import snowflake.connector
 import urllib.error 
-#from  urllib.error import URLERROR
+import URLERROR
 
 
 
@@ -48,12 +48,8 @@ try:
     streamlit.error("Please select the fruit to get information:")
 
   else:
-
-    #Let's removed the line of raw JSON, and separate the base URL from the fruit name (which will make it easier to use a variable there).
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # take the json version of the response and normalize it 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # output it on the screen as a table
     streamlit.dataframe(fruityvice_normalized)
 
 except URLERROR as e:
@@ -73,6 +69,7 @@ if streamlit.button ('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+  
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT * from fruit_load_list")
 
