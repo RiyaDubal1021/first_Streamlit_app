13cfaaabd2df00ccb0667a0fd6126f5afe658b55
RiyaDubal1021 <137475989+RiyaDubal1021@users.noreply.github.com>
2023-07-07 13:46:42 -0700

    Update Streamlit_app.py
    clean up

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -36,18 +36,31 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # A new section to display fruityvice advise
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
+#streamlit.write('The user entered ', fruit_choice)
+
+#New secttion to display frityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
+if not fruit_choice
+streamlit.error("Please select the fruit to get information:")
+
+else:
+
 #Let's removed the line of raw JSON, and separate the base URL from the fruit name (which will make it easier to use a variable there).
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json())
-
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+except URLERROR as e:
+streamlit.error()
+
+#streamlit.text(fruityvice_response.json())
+
 
 streamlit.stop()
 #query meta data
